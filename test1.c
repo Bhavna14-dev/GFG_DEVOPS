65db9633dc8071027904455163f2286848af78aa
bhavna bisht <bhavnabisht95@gmail.com>
2024-03-01 20:57:25 +0530

    statement added

--- test1.c
+++ test1.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int sum(int a, int n)
 {
@@ -14,22 +13,4 @@ int mul(int a , int b)
     return a*b;
 }
 
-void test ()
-{
-    printf("test");
-=======
-#include <stdio.h>
-int sum(int a, int n)
-{
-    return a + n;
-}
-
-int diff(int a, int b)
-{
-    return a -b;
-}
-int mul(int a , int b)
-{
-    return a*b;
->>>>>>> c0b6c64644d3fbb21caafbe88f978135b10172fd
-}
+hello world !!!
